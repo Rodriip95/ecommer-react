06d2f14e5b8e9e762505b6afe972eff92efd68f3
rodriip95 <rodrigo.m.penela@gmail.com>
2020-11-04 20:39:09 -0300

    Reformula useParams

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, useParams} from 'react-router-dom';
 
 import './stylesComponents.css'
 
 
 
 export default function Item({producto}){
+    const {id} = useParams();
     return(
         <div id="productCard" className="col-2 m-1">
             <h4>{producto.articulo}</h4>
             <p>
                 $ {producto.precio}
             </p>
-            <Link to={`/${producto.articulo}`}>Ver Producto</Link>
+            <Link to={`/item/${producto.id}`}>Ver Producto</Link>
         </div>
     )
 }
